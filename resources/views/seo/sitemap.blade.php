b2fc7d08b62ce9a70c883273b25b3d8ab90bf4c2
Md Jewel <jewelbcse@gmail.com>
2026-06-13 12:12:37 +0600

    sitemap version update blade

--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -1,22 +1,16 @@
-{{-- sitemap.blade.php --}}
-
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!} 
+{{-- resources/views/sitemap.blade.php --}}
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($urls as $url)
     <url>
-        <loc>
-            {{ $url['loc'] }}
+        <loc>{{ $url['loc'] }}</loc>
 
-        </loc>
         @if(!empty($url['lastmod']))
             <lastmod>{{ $url['lastmod'] }}</lastmod>
         @endif
-        <changefreq>
-            {{ $url['changefreq'] }}
-        </changefreq>
-        <priority>
-            {{ $url['priority'] }}
-        </priority>
+
+        <changefreq>{{ $url['changefreq'] ?? 'weekly' }}</changefreq>
+        <priority>{{ $url['priority'] ?? '0.8' }}</priority>
     </url>
 @endforeach
 </urlset>
