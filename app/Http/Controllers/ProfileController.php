74e14be20f82ac3de5b63c6cd2268da65a0154e2
Md Jewel <jewelbcse@gmail.com>
2026-07-05 14:03:07 +0600

    Unable to locate a class or view for component [input-label] solve.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,7 +13,7 @@ class ProfileController extends Controller
 {
     public function edit(Request $request): View
     {
-        return view('profile.edit', [
+        return view('profile.doctorProfile', [
             'user' => $request->user(),
         ]);
     }
@@ -28,7 +28,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.doctorProfile')->with('status', 'profile-updated');
     }
 
     public function destroy(Request $request): RedirectResponse
